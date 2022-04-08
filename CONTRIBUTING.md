59b6335a54ac2233f9b28590b30231faed6b63db
Carolyn Van Slyck <me@carolynvanslyck.com>
2022-04-08 15:33:23 -0500

    Build the examples in CI
    When a pull request is created or merged, build all the example bundles.
    
    I had to fix a few bundles to get them building properly:
    * azure-keyvault - force building to amd64 since powershell requires
      different commands to build on arm
    * Add missing schemaVersion to some examples
    * Specify custom porter build arguments for the private-assets command
      to pass in a secret.
    
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -7,3 +7,17 @@ In your pull request:
 1. Put your porter.yaml and supporting files in that directory.
 1. Include a README.md that explains what the example demonstrates, what the bundle does, and how to use the bundle.
 1. Include two tables: one for parameters and another for credentials. This helps people see info about the bundle without having to run porter explain.
+
+## Build an example
+
+To build an example just like we do in CI,
+
+```
+mage BuildExample NAME
+```
+
+Run the following command to build all examples:
+
+```
+mage Build
+```
---
+++ magefile.go
@@ -0,0 +1,93 @@
+//go:build mage
+// +build mage
+
+// This is a magefile, and is a "makefile for go".
+// See https://magefile.org/
+package main
+
+import (
+	"fmt"
+	"get.porter.sh/magefiles/porter"
+	"golang.org/x/sync/errgroup"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	// mage:import
+	_ "get.porter.sh/magefiles/ci"
+	"github.com/carolynvs/magex/shx"
+	"github.com/hashicorp/go-multierror"
+	"github.com/magefile/mage/mg"
+)
+
+func Build() {
+	mg.SerialDeps(BuildExamples)
+}
+
+// BuildExamples builds every example bundle
+func BuildExamples() error {
+	results, err := ioutil.ReadDir(".")
+	if err != nil {
+		return err
+	}
+
+	var bigErr *multierror.Error
+	for _, result := range results {
+		if result.IsDir() {
+			bundleName := result.Name()
+			if _, err := os.Stat(filepath.Join(bundleName, "porter.yaml")); err == nil {
+				err := BuildExample(bundleName)
+				if err != nil {
+					// Keep trying to build all the bundles, don't stop on the first one
+					bigErr = multierror.Append(bigErr, fmt.Errorf("error building bundle %s: %w", bundleName, err))
+					continue
+				}
+			}
+		}
+	}
+
+	return bigErr.ErrorOrNil()
+}
+
+// BuildExample builds the specified example bundle
+func BuildExample(name string) error {
+	mg.SerialDeps(installMixins)
+
+	fmt.Println("\n==========================")
+	fmt.Printf("Building example bundle: %s\n", name)
+
+	if customBuildFlags, err := ioutil.ReadFile(filepath.Join(name, "build-args.txt")); err == nil {
+		customBuildArgs := strings.Split(string(customBuildFlags), " ")
+		buildArgs := append([]string{"build"}, customBuildArgs...)
+		return shx.Command("porter", buildArgs...).
+			CollapseArgs().In(name).RunV()
+	}
+	// Always build for amd64 even if on an arm host
+	// This is a bit of a hack until we have multi-arch support
+	return shx.Command("porter", "build").
+		In(name).Env("DOCKER_DEFAULT_PLATFORM=linux/amd64").RunV()
+}
+
+func installMixins() error {
+	mg.SerialDeps(porter.UseBinForPorterHome, porter.EnsurePorter)
+
+	mixins := []porter.InstallMixinOptions{
+		{Name: "arm"},
+		{Name: "az"},
+		{Name: "docker"},
+		{Name: "docker-compose"},
+		{Name: "exec"},
+		{Name: "helm3", Feed: "https://mchorfa.github.io/porter-helm3/atom.xml", Version: "v0.1.14"},
+		{Name: "kubernetes"},
+		{Name: "terraform"},
+	}
+	var errG errgroup.Group
+	for _, mixin := range mixins {
+		mixin := mixin
+		errG.Go(func() error {
+			return porter.EnsureMixin(mixin)
+		})
+	}
+	return errG.Wait()
+}
